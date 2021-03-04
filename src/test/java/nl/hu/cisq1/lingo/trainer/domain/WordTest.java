9d5074a69daba48f7951ffb34b0a758c0ede9ee6
Bosshi <S.Shirre@hotmail.com>
2021-03-04 12:31:54 +0100

    Improved word coverage

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/WordTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/WordTest.java
@@ -3,6 +3,11 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidWordLengthException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,12 +22,23 @@ class WordTest {
         assertEquals(5, length);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideWordExamples")
     @DisplayName("exception is thrown when the word length is not 5, 6 or 7")
-    void wordLengthNotAllowed() {
+    void wordLengthNotAllowed(String word) {
         assertThrows(
                 InvalidWordLengthException.class,
-                () -> new Word("Tafelkleed")
+                () -> new Word(word)
         );
     }
+
+    private static Stream<Arguments> provideWordExamples() {
+        return Stream.of(
+                Arguments.of("kat"),
+                Arguments.of("meer"),
+                Arguments.of("kaasboer"),
+                Arguments.of("wafelhuis")
+        );
+    }
+
 }
